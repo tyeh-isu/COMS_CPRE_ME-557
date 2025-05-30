27fa71bb73a039801bf00e4e0013c962c2834e9d
Tsung Pin Yeh <tyeh@iastate.edu>
2025-05-29 21:17:21 -0500

    Add files via upload

---
+++ 557-11-3D_Coordinate/my_application.cpp
@@ -0,0 +1,128 @@
+#include "my_application.h"
+
+// Render system
+#include "my_simple_render_factory.h"
+
+// use radian rather degree for angle
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+// Std
+#include <stdexcept>
+#include <array>
+
+MyApplication::MyApplication()
+{
+    _loadGameObjects();
+}
+
+void MyApplication::run() 
+{
+    MySimpleRenderFactory simpleRenderFactotry{ m_myDevice, m_myRenderer.swapChainRenderPass() };
+
+    while (!m_myWindow.shouldClose()) 
+    {
+        // Note: depending on the platform (Windows, Linux or Mac), this function
+        // will cause the event proecssing to block during a Window move, resize or
+        // menu operation. Users can use the "window refresh callback" to redraw the
+        // contents of the window when necessary during such operation.
+        m_myWindow.pollEvents();
+		
+        // Please note that commandBuffer could be null pointer
+        // if the swapChain needs to be recreated
+        if (auto commandBuffer = m_myRenderer.beginFrame())
+		{
+            // In case we have multiple render passes for the current frame
+            // begin offsreen shadow pass
+            // render shadow casting objects
+            // end offscreen shadow pass
+
+            m_myRenderer.beginSwapChainRenderPass(commandBuffer);
+            simpleRenderFactotry.renderGameObjects(commandBuffer, m_vMyGameObjects);
+            m_myRenderer.endSwapChainRenderPass(commandBuffer);
+
+            m_myRenderer.endFrame();
+        }
+    }
+
+    // GPU will block until all CPU is complete
+    vkDeviceWaitIdle(m_myDevice.device());
+}
+
+std::unique_ptr<MyModel> createCubeModel(MyDevice& device, glm::vec3 offset)
+{
+    // Unit cube
+    std::vector<MyModel::Vertex> vertices
+    {
+       // left face (white)
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+
+      // right face (yellow)
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+
+      // top face (orange, remember y axis points down)
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+
+      // bottom face (red)
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+
+      // front face (blue)
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+
+      // back face (green)
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    };
+
+    for (auto& v : vertices)
+    {
+        v.position += offset;
+    }
+
+    return std::make_unique<MyModel>(device, vertices);
+}
+
+void MyApplication::_loadGameObjects()
+{
+    std::shared_ptr<MyModel> myModel = createCubeModel(m_myDevice, { .0f, .0f, .0f });
+    
+    // Note: +X to the right, +Y down and +Z inside the screen
+    auto cube = MyGameObject::createGameObject();
+    cube.model = myModel;
+    cube.transform.translation = { 0.0f, 0.0f, 0.5f };
+    cube.transform.scale = { 1.0f, 1.0f, 1.0f };
+
+    m_vMyGameObjects.push_back(std::move(cube));
+}
+
