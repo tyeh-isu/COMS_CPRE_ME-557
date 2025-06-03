58d0476739c548518edc9942858a45ded0e9f53e
Tsung Pin Yeh <tyeh@iastate.edu>
2025-06-02 21:33:46 -0500

    Add files via upload

---
+++ 557-20-3D_PointLight/my_game_object.h
@@ -0,0 +1,51 @@
+#ifndef __MY_GAMEOBJECT_H__
+#define __MY_GAMEOBJECT_H__
+
+#include "my_model.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+// std
+#include <memory>
+
+struct TransformComponent
+{
+	glm::vec3 translation{ 0.0f, 0.0f, 0.0f };
+	glm::vec3 scale{ 1.0f, 1.0f, 1.0f };
+	glm::vec3 rotation{ 0.0f, 0.0f, 0.0f };
+
+	// Matrix corresponds to the overall transformation - scale * Rz * Rx * Ry * transform (row, pitch, yaw)
+	// Rotation conventation uses Tait-Bryan angles with axis order Y(1), X(2), Z(3)
+    // https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
+	glm::mat4 mat4();
+
+	glm::mat3 normalMatrix();
+};
+
+class MyGameObject
+{
+public:
+	using id_t = unsigned int;
+
+	static MyGameObject createGameObject()
+	{
+		static id_t currentID = 0;
+		return MyGameObject{ currentID++ };
+	}
+
+	MyGameObject(const MyGameObject&) = delete;
+	MyGameObject& operator=(const MyGameObject&) = delete;
+	MyGameObject(MyGameObject&&) = default;
+	MyGameObject& operator=(MyGameObject&&) = default;
+
+	id_t                     getID() const { return m_iID; }
+	std::shared_ptr<MyModel> model{};
+	glm::vec3                color{};
+	TransformComponent       transform{};
+
+private:
+	MyGameObject(id_t objID) : m_iID{ objID } {}
+	id_t m_iID;
+};
+
+#endif
+
