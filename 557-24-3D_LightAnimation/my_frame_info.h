3091d449c8e279e4a6b7f0e1eb757c060c48891d
Tsung Pin Yeh <tyeh@iastate.edu>
2025-06-03 21:20:55 -0500

    Add files via upload

---
+++ 557-24-3D_LightAnimation/my_frame_info.h
@@ -0,0 +1,38 @@
+#ifndef __MY_FRAMEINFO_H__
+#define __MY_FRAMEINFO_H__
+
+#include "my_camera.h"
+#include "my_game_object.h"
+
+// lib
+#include <vulkan/vulkan.h>
+
+struct MyPointLight 
+{
+	glm::vec4 position{ -1.0f, 1.0f, 1.0f, 1.0f};     // ignore w
+	glm::vec4 color{1.0f, 1.0f, 1.0f, 1.0f};          // w is intensity
+};
+
+struct MyGlobalUBO 
+{
+    alignas(16) glm::mat4 projection{ 1.0f };
+    alignas(16) glm::mat4 view{ 1.0f };
+    alignas(16) glm::vec4 ambientLightColor{ 1.0f, 1.0f, 1.0f, 0.01f }; // w is intensity
+    alignas(16) glm::vec3 lightPosition{ -1.0f, 1.0f, 1.0 };
+    alignas(16) glm::vec4 lightColor{ 1.0f, 1.0f, 1.0f, 1.0f };         // w is light intensity
+
+    // and we can add more here...
+};
+
+struct MyFrameInfo
+{
+	int                frameIndex;
+	float              frameTime;
+	VkCommandBuffer    commandBuffer;
+	MyCamera&          camera;
+	VkDescriptorSet    globalDescriptorSet;
+	MyGameObject::Map& gameObjects;
+};
+
+#endif
+
