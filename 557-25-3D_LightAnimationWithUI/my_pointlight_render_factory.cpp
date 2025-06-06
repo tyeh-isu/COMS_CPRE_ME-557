734812daa207e12172e08bcec1704542349cb200
Tsung Pin Yeh <tyeh@iastate.edu>
2025-06-05 21:22:08 -0500

    Add files via upload

---
+++ 557-25-3D_LightAnimationWithUI/my_pointlight_render_factory.cpp
@@ -0,0 +1,92 @@
+#include "my_pointlight_render_factory.h"
+
+// libs
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+// std
+#include <array>
+#include <cassert>
+#include <stdexcept>
+
+MyPointLightRenderFactory::MyPointLightRenderFactory(
+    MyDevice& device, VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout)
+    : m_myDevice{ device }
+{
+    _createPipelineLayout(globalSetLayout);
+    _createPipeline(renderPass);
+}
+
+MyPointLightRenderFactory::~MyPointLightRenderFactory()
+{
+    vkDestroyPipelineLayout(m_myDevice.device(), m_vkPipelineLayout, nullptr);
+}
+
+void MyPointLightRenderFactory::_createPipelineLayout(VkDescriptorSetLayout globalSetLayout)
+{
+    // VkPushConstantRange pushConstantRange{};
+    // pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+    // pushConstantRange.offset = 0;
+    // pushConstantRange.size = sizeof(SimplePushConstantData);
+
+    std::vector<VkDescriptorSetLayout> descriptorSetLayouts{ globalSetLayout };
+
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(descriptorSetLayouts.size());
+    pipelineLayoutInfo.pSetLayouts = descriptorSetLayouts.data();
+    pipelineLayoutInfo.pushConstantRangeCount = 0;
+    pipelineLayoutInfo.pPushConstantRanges = nullptr;
+    if (vkCreatePipelineLayout(m_myDevice.device(), &pipelineLayoutInfo, nullptr, &m_vkPipelineLayout) !=
+        VK_SUCCESS) {
+        throw std::runtime_error("failed to create pipeline layout!");
+    }
+}
+
+void MyPointLightRenderFactory::_createPipeline(VkRenderPass renderPass) 
+{
+    assert(m_vkPipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
+
+    PipelineConfigInfo pipelineConfig{};
+    MyPipeline::defaultPipelineConfigInfo(pipelineConfig);
+
+    // We only need for simple render system because
+    // we create the vertex in the vertex shader for point light
+    pipelineConfig.attributeDescriptions.clear(); 
+    pipelineConfig.bindingDescriptions.clear();
+
+    pipelineConfig.renderPass = renderPass;
+    pipelineConfig.pipelineLayout = m_vkPipelineLayout;
+
+    m_pMyPipeline = std::make_unique<MyPipeline>(
+        m_myDevice,
+        "shaders/point_light.vert.spv",
+        "shaders/point_light.frag.spv",
+        pipelineConfig);
+}
+
+void MyPointLightRenderFactory::update(MyGlobalUBO& ubo, const glm::vec3 offset)
+{
+    // Update light position to UBO
+    ubo.pointLight.position += glm::vec4(offset, 1.0f);
+}
+
+void MyPointLightRenderFactory::render(MyFrameInfo& frameInfo) 
+{
+    m_pMyPipeline->bind(frameInfo.commandBuffer);
+
+    vkCmdBindDescriptorSets(
+        frameInfo.commandBuffer,
+        VK_PIPELINE_BIND_POINT_GRAPHICS,
+        m_vkPipelineLayout,
+        0,
+        1,
+        &frameInfo.globalDescriptorSet,
+        0,
+        nullptr);
+
+    vkCmdDraw(frameInfo.commandBuffer, 6, 1, 0, 0);
+}
+
