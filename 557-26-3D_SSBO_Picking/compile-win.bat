e6053b4dfa6f257f28ab8cd49d9f95f1a0cb6fef
Tsung Pin Yeh <tyeh@iastate.edu>
2025-06-06 15:34:27 -0500

    Delete 557-26-3D_SSBO_Picking directory

--- 557-26-3D_SSBO_Picking/compile-win.bat
+++
@@ -1,6 +0,0 @@
-%VULKAN_SDK%/bin/glslc.exe ./shaders/simple_shader.vert -o ./shaders/simple_shader.vert.spv
-%VULKAN_SDK%/bin/glslc.exe ./shaders/simple_shader.frag -o ./shaders/simple_shader.frag.spv
-%VULKAN_SDK%/bin/glslc.exe ./shaders/point_light.vert -o ./shaders/point_light.vert.spv
-%VULKAN_SDK%/bin/glslc.exe ./shaders/point_light.frag -o ./shaders/point_light.frag.spv
-%VULKAN_SDK%/bin/glslc.exe ./shaders/pick_shader.vert -o ./shaders/pick_shader.vert.spv
-%VULKAN_SDK%/bin/glslc.exe ./shaders/pick_shader.frag -o ./shaders/pick_shader.frag.spv
