acca6eafba4c40a4f7c429bbc6ba80f79c55b1ed
Tsung Pin Yeh <tyeh@iastate.edu>
2025-06-15 12:28:29 -0500

    Delete 557-27-3D_SimpleTextureKTX directory

--- 557-27-3D_SimpleTextureKTX/compile-win.bat
+++
@@ -1,2 +0,0 @@
-%VULKAN_SDK%/bin/glslc.exe ./shaders/texture_shader.vert -o ./shaders/texture_shader.vert.spv
-%VULKAN_SDK%/bin/glslc.exe ./shaders/texture_shader.frag -o ./shaders/texture_shader.frag.spv
