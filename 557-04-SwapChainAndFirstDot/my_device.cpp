b86a7c4f958bf6b3220dda75fd410a9a308803b6
Yeh, Tsung-Pin <tsung-pin.yeh@siemens.com>
2025-06-14 20:43:07 -0500

    Changed render engine name

--- 557-04-SwapChainAndFirstDot/my_device.cpp
+++ 557-04-SwapChainAndFirstDot/my_device.cpp
@@ -88,7 +88,7 @@ void MyDevice::_createInstance()
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "My Vulkan App";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
+    appInfo.pEngineName = "My Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
     
